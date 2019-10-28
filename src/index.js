93713fe6565b179f2364d2f6d142af79ee878974
William Muli <willi.wambu@gmail.com>
2019-10-28 23:46:37 +0300

    Configure redux store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './components/App';
 import setGlobalStyles from 'styles/globals';
+import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker';
+import configureStore from './store';
 
 setGlobalStyles();
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+const renderApp = () => {
+	const initialState = {};
+
+	const store = configureStore(initialState);
+
+	store.dispatch({ type: '@hnClone/@@INIT' });
+
+	ReactDOM.render(
+		<Provider store={store}>
+			<Index />
+		</Provider>,
+		document.getElementById('root')
+	);
+};
+
+renderApp();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
